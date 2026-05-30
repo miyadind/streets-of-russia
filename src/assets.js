b7f7aee09e8fbc8a5baab328c3c67405738b17bc
miyadind <miyadind@gmail.com>
2026-05-29 18:27:40 -0700

    Add asset registry

---
+++ src/assets.js
@@ -0,0 +1,17 @@
+window.Assets = {
+  backgrounds:{
+    main:'assets/backgrounds/Main.png',
+    level1:['assets/backgrounds/street01.png','assets/backgrounds/street02.png','assets/backgrounds/street03.png']
+  },
+  boris:{
+    idle:'assets/characters/boris/Idle.png',
+    walk:['assets/characters/boris/walk0.png','assets/characters/boris/walk1.png','assets/characters/boris/walk2.png'],
+    punch:['assets/characters/boris/punch0.png','assets/characters/boris/punch1.png','assets/characters/boris/punch2.png']
+  },
+  dog:{
+    idle:'assets/enemies/dog-regime/idle.png',
+    walk:['assets/enemies/dog-regime/walk0.png','assets/enemies/dog-regime/walk1.png'],
+    attack:['assets/enemies/dog-regime/punch0.png','assets/enemies/dog-regime/punch1.png'],
+    dead:'assets/enemies/dog-regime/dead.png'
+  }
+};
