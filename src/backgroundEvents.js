1757b75270c1d06c83555744d3f6e6d2fd7e867c
miyadind <miyadind@gmail.com>
2026-06-02 00:21:11 -0700

    Add street02 poster event background path

---
+++ src/backgroundEvents.js
@@ -0,0 +1,6 @@
+// Background variants used by level-specific story events.
+window.Assets = window.Assets || {};
+Assets.backgrounds = Assets.backgrounds || {};
+Assets.backgrounds.alternate = Assets.backgrounds.alternate || {};
+
+Assets.backgrounds.alternate.street02 = 'assets/backgrounds/street02-2.png';
