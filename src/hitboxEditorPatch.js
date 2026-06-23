1c8c195f9985730a9aba64c741c92237ba34c2e7
miyadind <miyadind@gmail.com>
2026-06-22 19:10:48 -0700

    Simplify hitboxes to body attack pushbox

--- src/hitboxEditorPatch.js
+++ src/hitboxEditorPatch.js
@@ -2,62 +2,68 @@
   if (typeof GAME_CONFIG === 'undefined') return;
 
   const COLORS = {
-    body: '#00d6d6',
-    hurt: '#ff3030',
+    body: '#ff3030',
+    pushbox: '#ffd500',
     attack1: '#45ff45',
     attack2: '#45ff45',
     attack3: '#45ff45',
-    club: '#ffd500',
-    slide: '#00ffff',
-    counter: '#8f7cff'
+    attack: '#45ff45',
+    slideAttack: '#45ff45',
+    biteAttack: '#45ff45'
   };
 
   const DEFAULT_HITBOXES = {
     heroes: {
       boris: {
         body: { x: -34, y: -132, w: 68, h: 132 },
+        pushbox: { x: -28, y: -20, w: 56, h: 36 },
         attack1: { x: 22, y: -118, w: 46, h: 40 },
         attack2: { x: 22, y: -118, w: 52, h: 40 },
-        attack3: { x: 22, y: -118, w: 62, h: 40 },
-        counter: { x: 0, y: -58, w: 150, h: 116 }
+        attack3: { x: 22, y: -118, w: 62, h: 40 }
       },
       alexey: {
         body: { x: -34, y: -132, w: 68, h: 132 },
+        pushbox: { x: -28, y: -20, w: 56, h: 36 },
         attack1: { x: 22, y: -118, w: 46, h: 40 },
         attack2: { x: 22, y: -118, w: 52, h: 40 },
-        attack3: { x: 22, y: -118, w: 62, h: 40 },
-        counter: { x: 0, y: -58, w: 150, h: 116 }
+        attack3: { x: 22, y: -118, w: 62, h: 40 }
       },
       anna: {
-        body: { x: -34, y: -132, w: 68, h: 132 },
-        attack1: { x: 22, y: -118, w: 46, h: 40 },
-        attack2: { x: 22, y: -118, w: 52, h: 40 },
-        attack3: { x: 22, y: -118, w: 62, h: 40 },
-        counter: { x: 0, y: -58, w: 150, h: 116 }
+        body: { x: -30, y: -124, w: 60, h: 124 },
+        pushbox: { x: -24, y: -18, w: 48, h: 34 },
+        attack1: { x: 20, y: -108, w: 42, h: 36 },
+        attack2: { x: 20, y: -108, w: 48, h: 36 },
+        attack3: { x: 20, y: -108, w: 56, h: 36 }
       }
     },
     enemies: {
       dogRegime: {
-        hurt: { x: -38, y: -140, w: 76, h: 140 },
-        body: { x: -42, y: -20, w: 84, h: 40 },
-        club: { x: 28, y: -42, w: 54, h: 84 }
+        body: { x: -38, y: -140, w: 76, h: 140 },
+        pushbox: { x: -42, y: -20, w: 84, h: 40 },
+        attack: { x: 28, y: -92, w: 58, h: 46 }
       },
       zetnik: {
-        hurt: { x: -38, y: -140, w: 76, h: 140 },
-        body: { x: -42, y: -20, w: 84, h: 40 },
-        club: { x: 28, y: -42, w: 54, h: 84 }
+        body: { x: -38, y: -140, w: 76, h: 140 },
+        pushbox: { x: -42, y: -20, w: 84, h: 40 },
+        attack: { x: 28, y: -92, w: 58, h: 46 }
       },
       sucker: {
-        hurt: { x: -44, y: -150, w: 88, h: 150 },
-        body: { x: -52, y: -24, w: 104, h: 48 },
-        slide: { x: 8, y: -112, w: 88, h: 54 }
+        body: { x: -44, y: -150, w: 88, h: 150 },
+        pushbox: { x: -52, y: -24, w: 104, h: 48 },
+        slideAttack: { x: 8, y: -112, w: 88, h: 54 },
+        biteAttack: { x: 18, y: -112, w: 62, h: 54 }
       },
       bastard: {
-        hurt: { x: -38, y: -132, w: 76, h: 132 },
-        body: { x: -44, y: -20, w: 88, h: 40 }
+        body: { x: -38, y: -132, w: 76, h: 132 },
+        pushbox: { x: -44, y: -20, w: 88, h: 40 },
+        attack: { x: 26, y: -92, w: 54, h: 44 }
       }
     }
   };
+
+  function clone(value) {
+    return JSON.parse(JSON.stringify(value));
+  }
 
   function mergeMissing(target, source) {
     for (const key of Object.keys(source)) {
@@ -70,9 +76,33 @@
     }
   }
 
+  function migrateHitboxEntity(target, defaults) {
+    if (!target || !defaults) return clone(defaults);
+    if (target.hurt && !target.body) target.body = clone(target.hurt);
+    if (target.club && !target.attack) target.attack = clone(target.club);
+    if (target.slide && !target.slideAttack) target.slideAttack = clone(target.slide);
+    delete target.hurt;
+    delete target.club;
+    delete target.counter;
+    delete target.slide;
+    mergeMissing(target, defaults);
+    for (const key of Object.keys(target)) {
+      if (!defaults[key]) delete target[key];
+    }
+    return target;
+  }
+
   function ensureHitboxes() {
     if (!GAME_CONFIG.hitboxes) GAME_CONFIG.hitboxes = {};
-    mergeMissing(GAME_CONFIG.hitboxes, DEFAULT_HITBOXES);
+    if (!GAME_CONFIG.hitboxes.heroes) GAME_CONFIG.hitboxes.heroes = {};
+    if (!GAME_CONFIG.hitboxes.enemies) GAME_CONFIG.hitboxes.enemies = {};
+
+    for (const key of Object.keys(DEFAULT_HITBOXES.heroes)) {
+      GAME_CONFIG.hitboxes.heroes[key] = migrateHitboxEntity(GAME_CONFIG.hitboxes.heroes[key], DEFAULT_HITBOXES.heroes[key]);
+    }
+    for (const key of Object.keys(DEFAULT_HITBOXES.enemies)) {
+      GAME_CONFIG.hitboxes.enemies[key] = migrateHitboxEntity(GAME_CONFIG.hitboxes.enemies[key], DEFAULT_HITBOXES.enemies[key]);
+    }
   }
 
   function mirrorBox(box) {
@@ -80,6 +110,7 @@
   }
 
   function worldBox(anchorX, anchorY, facing, box) {
+    if (!box) return null;
     const b = facing === -1 ? mirrorBox(box) : box;
     return { x: anchorX + b.x, y: anchorY + b.y, w: b.w, h: b.h };
   }
@@ -110,18 +141,13 @@
     ctx.restore();
   }
 
+  function boxOverlap(a, b) {
+    return a && b && a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.y + a.h > b.y;
+  }
+
   ensureHitboxes();
 
-  if (typeof Player !== 'undefined' && !Player.prototype.hitboxEditorPatchApplied) {
-    const originalGetAttackData = Player.prototype.getAttackData;
-    Player.prototype.getAttackData = function () {
-      const data = originalGetAttackData.call(this);
-      const key = this.comboStep === 3 ? 'attack3' : this.comboStep === 2 ? 'attack2' : 'attack1';
-      const box = heroBox(this.heroKey, key);
-      if (box) data.range = box.w;
-      return data;
-    };
-
+  if (typeof Player !== 'undefined') {
     Player.prototype.getHitbox = function () {
       const key = this.comboStep === 3 ? 'attack3' : this.comboStep === 2 ? 'attack2' : 'attack1';
       return worldBox(this.x, this.y, this.facing, heroBox(this.heroKey, key));
@@ -131,84 +157,124 @@
       return worldBox(this.x, this.y, this.facing, heroBox(this.heroKey, 'body'));
     };
 
+    Player.prototype.getPushbox = function () {
+      return worldBox(this.x, this.y, this.facing, heroBox(this.heroKey, 'pushbox'));
+    };
+
     Player.prototype.getCounterBox = function () {
-      return worldBox(this.x, this.y, this.facing, heroBox(this.heroKey, 'counter'));
+      return null;
     };
 
-    const originalCanCounterSlide = Player.prototype.canCounterSlide;
-    Player.prototype.canCounterSlide = function (enemy) {
-      if (this.state !== 'attack') return false;
-      if (!enemy || typeof enemy.getHurtbox !== 'function') return false;
-      const dy = Math.abs((enemy.y || 0) - this.y);
-      const counter = this.getCounterBox ? this.getCounterBox() : null;
-      return dy <= ((counter && counter.h / 2) || GAME_CONFIG.enemyAttackRangeY) && Combat.overlap(counter, enemy.getHurtbox());
+    Player.prototype.canCounterSlide = function () {
+      return false;
     };
 
-    const originalPlayerDraw = Player.prototype.draw;
+    const originalPlayerDraw = Player.prototype.hitboxSimpleDrawOriginal || Player.prototype.draw;
+    Player.prototype.hitboxSimpleDrawOriginal = originalPlayerDraw;
     Player.prototype.draw = function (ctx, debug = false) {
-      originalPlayerDraw.call(this, ctx, debug);
+      originalPlayerDraw.call(this, ctx, false);
       if (!debug) return;
       strokeBox(ctx, this.getBodyBox(), COLORS.body, 'BODY');
-      if (this.state === 'attack') {
-        strokeBox(ctx, this.getHitbox(), COLORS.attack1, 'ATTACK');
-        strokeBox(ctx, this.getCounterBox(), COLORS.counter, 'COUNTER');
-      }
+      strokeBox(ctx, this.getPushbox(), COLORS.pushbox, 'PUSHBOX');
+      if (this.state === 'attack') strokeBox(ctx, this.getHitbox(), COLORS.attack1, 'ATTACK');
     };
-
-    Player.prototype.hitboxEditorPatchApplied = true;
   }
 
-  if (typeof DogRegimeEnemy !== 'undefined' && !DogRegimeEnemy.prototype.hitboxEditorPatchApplied) {
-    DogRegimeEnemy.prototype.getHurtbox = function () {
-      return worldBox(this.x, this.y, this.facing, enemyBox(this.enemyType, 'hurt'));
-    };
-
-    DogRegimeEnemy.prototype.getGroundBodyBox = function () {
+  if (typeof DogRegimeEnemy !== 'undefined') {
+    DogRegimeEnemy.prototype.getBodyBox = function () {
       return worldBox(this.x, this.y, this.facing, enemyBox(this.enemyType, 'body'));
     };
 
+    DogRegimeEnemy.prototype.getHurtbox = function () {
+      return this.getBodyBox();
+    };
+
+    DogRegimeEnemy.prototype.getPushbox = function () {
+      return worldBox(this.x, this.y, this.facing, enemyBox(this.enemyType, 'pushbox'));
+    };
+
+    DogRegimeEnemy.prototype.getGroundBodyBox = function () {
+      return this.getPushbox();
+    };
+
+    DogRegimeEnemy.prototype.getAttackBox = function () {
+      return worldBox(this.x, this.y, this.facing, enemyBox(this.enemyType, 'attack'));
+    };
+
     DogRegimeEnemy.prototype.getClubReachBox = function () {
-      const b = enemyBox(this.enemyType, 'club');
-      if (!b) return null;
-      return worldBox(this.x, this.y, this.facing, b);
+      return this.getAttackBox();
     };
 
     DogRegimeEnemy.prototype.canClubReachPlayer = function (player, anticipation = false) {
       if (!player || typeof player.getBodyBox !== 'function') return false;
-      const reach = this.getClubReachBox && this.getClubReachBox();
-      if (!reach) return false;
-      const pad = anticipation ? 4 : 0;
-      const paddedReach = { x: reach.x - pad, y: reach.y - pad, w: reach.w + pad * 2, h: reach.h + pad * 2 };
-      return Combat.overlap(paddedReach, player.getBodyBox());
+      const attack = this.getAttackBox();
+      if (!anticipation) return boxOverlap(attack, player.getBodyBox());
+      const pad = 4;
+      return boxOverlap({ x: attack.x - pad, y: attack.y - pad, w: attack.w + pad * 2, h: attack.h + pad * 2 }, player.getBodyBox());
     };
 
-    const originalEnemyDraw = DogRegimeEnemy.prototype.draw;
+    DogRegimeEnemy.prototype.isInAttackRange = function (player) {
+      return this.canClubReachPlayer(player, true);
+    };
+
+    DogRegimeEnemy.prototype.updateAttack = function (dt, scene) {
+      this.attackTimer += dt;
+      const activeStart = GAME_CONFIG.enemyWindupMs;
+      const activeEnd = GAME_CONFIG.enemyWindupMs + GAME_CONFIG.enemyActiveMs;
+
+      if (!this.attackHasHit && this.attackTimer >= activeStart && this.attackTimer <= activeEnd) {
+        const player = scene.player;
+        if (this.canClubReachPlayer(player, false)) {
+          const hit = player.receiveDamage(this.damage, { source: 'melee', knockbackX: this.facing * 18 });
+          if (hit) scene.hitStop = 42;
+        }
+        this.attackHasHit = true;
+      }
+
+      if (this.attackTimer >= GAME_CONFIG.enemyWindupMs + GAME_CONFIG.enemyActiveMs + GAME_CONFIG.enemyRecoveryMs) {
+        this.state = 'walk';
+        this.intent = Math.random() < 0.58 ? 'retreat' : 'strafe';
+        this.strafeDirection = Math.random() < 0.5 ? -1 : 1;
+        this.retreatTimer = this.postAttackRetreatMs || 220;
+        const min = this.attackCooldownMinMs || 300;
+        const max = this.attackCooldownMaxMs || 520;
+        this.cooldown = min + Math.random() * Math.max(1, max - min);
+        this.attackTimer = 0;
+        this.attackHasHit = false;
+      }
+    };
+
+    const originalEnemyDraw = DogRegimeEnemy.prototype.hitboxSimpleDrawOriginal || DogRegimeEnemy.prototype.draw;
+    DogRegimeEnemy.prototype.hitboxSimpleDrawOriginal = originalEnemyDraw;
     DogRegimeEnemy.prototype.draw = function (ctx, debug = false) {
       originalEnemyDraw.call(this, ctx, false);
       if (!debug || !this.alive) return;
-      strokeBox(ctx, this.getHurtbox(), COLORS.hurt, 'HURT');
-      strokeBox(ctx, this.getGroundBodyBox(), COLORS.body, 'BODY');
-      if (this.enemyType === 'dogRegime' || this.enemyType === 'zetnik') strokeBox(ctx, this.getClubReachBox(), COLORS.club, 'CLUB');
+      strokeBox(ctx, this.getBodyBox(), COLORS.body, 'BODY');
+      strokeBox(ctx, this.getPushbox(), COLORS.pushbox, 'PUSHBOX');
+      if (this.state === 'attack') strokeBox(ctx, this.getAttackBox(), COLORS.attack, 'ATTACK');
     };
-
-    DogRegimeEnemy.prototype.hitboxEditorPatchApplied = true;
   }
 
-  if (typeof SuckerEnemy !== 'undefined' && !SuckerEnemy.prototype.hitboxEditorPatchApplied) {
+  if (typeof SuckerEnemy !== 'undefined') {
     SuckerEnemy.prototype.getSlideHitbox = function () {
-      return worldBox(this.x, this.slideY || this.y, this.facing, enemyBox('sucker', 'slide'));
+      return worldBox(this.x, this.slideY || this.y, this.facing, enemyBox('sucker', 'slideAttack'));
     };
 
-    const originalSuckerDraw = SuckerEnemy.prototype.draw;
+    SuckerEnemy.prototype.getBiteAttackBox = function () {
+      return worldBox(this.x, this.y, this.facing, enemyBox('sucker', 'biteAttack'));
+    };
+
+    const originalSuckerDraw = SuckerEnemy.prototype.hitboxSimpleDrawOriginal || SuckerEnemy.prototype.draw;
+    SuckerEnemy.prototype.hitboxSimpleDrawOriginal = originalSuckerDraw;
     SuckerEnemy.prototype.draw = function (ctx, debug = false) {
       originalSuckerDraw.call(this, ctx, debug);
-      if (debug && this.alive && this.state === 'slide') strokeBox(ctx, this.getSlideHitbox(), COLORS.slide, 'SLIDE');
+      if (!debug || !this.alive) return;
+      if (this.state === 'slide') strokeBox(ctx, this.getSlideHitbox(), COLORS.slideAttack, 'SLIDE_ATTACK');
+      if (this.state === 'pinBite') strokeBox(ctx, this.getBiteAttackBox(), COLORS.biteAttack, 'BITE_ATTACK');
     };
-
-    SuckerEnemy.prototype.hitboxEditorPatchApplied = true;
   }
 
-  if (typeof DevPanel !== 'undefined' && !DevPanel.hitboxEditorPatchApplied) {
+  if (typeof DevPanel !== 'undefined' && !DevPanel.hitboxEditorSimplePatchApplied) {
     if (!DevPanel.tabs.includes('HITBOXES')) DevPanel.tabs.push('HITBOXES');
     DevPanel.selectedHitboxEntityIndex = DevPanel.selectedHitboxEntityIndex || 0;
     DevPanel.selectedHitboxBoxIndex = DevPanel.selectedHitboxBoxIndex || 0;
@@ -264,7 +330,8 @@
       };
     };
 
-    const originalHandleClick = DevPanel.handleClick;
+    const originalHandleClick = DevPanel.hitboxOriginalHandleClick || DevPanel.handleClick;
+    DevPanel.hitboxOriginalHandleClick = originalHandleClick;
     DevPanel.handleClick = function (point, game) {
       const panel = this.panelRect();
       if (!this.inRect(point, panel)) return;
@@ -280,7 +347,6 @@
     DevPanel.handleHitboxEditorClick = function (point, game) {
       const r = this.hitboxEditorRects();
       const entity = this.getSelectedHitboxEntity();
-      const keys = this.hitboxBoxKeys(entity);
       if (this.inRect(point, r.entityPrev)) { this.selectedHitboxEntityIndex--; this.selectedHitboxBoxIndex = 0; this.setStatus('Entity changed'); return true; }
       if (this.inRect(point, r.entityNext)) { this.selectedHitboxEntityIndex++; this.selectedHitboxBoxIndex = 0; this.setStatus('Entity changed'); return true; }
       if (this.inRect(point, r.boxPrev)) { this.selectedHitboxBoxIndex--; this.setStatus('Box changed'); return true; }
@@ -294,8 +360,8 @@
         const plus = { x: r.box.x + 452, y: y - 22, w: 34, h: 28 };
         const bar = { x: r.box.x + 166, y: y - 13, w: 270, h: 12 };
         const prop = props[i];
-        const min = prop === 'w' || prop === 'h' ? 4 : -220;
-        const max = prop === 'w' || prop === 'h' ? 260 : 220;
+        const min = prop === 'w' || prop === 'h' ? 4 : -240;
+        const max = prop === 'w' || prop === 'h' ? 280 : 240;
         if (this.inRect(point, minus)) { hb[prop] = Math.max(min, hb[prop] - 2); this.applyToCurrentScene(game); this.setStatus(prop + ': ' + hb[prop]); return true; }
         if (this.inRect(point, plus)) { hb[prop] = Math.min(max, hb[prop] + 2); this.applyToCurrentScene(game); this.setStatus(prop + ': ' + hb[prop]); return true; }
         if (this.inRect(point, bar)) {
@@ -309,7 +375,8 @@
       return false;
     };
 
-    const originalDraw = DevPanel.draw;
+    const originalDraw = DevPanel.hitboxOriginalDraw || DevPanel.draw;
+    DevPanel.hitboxOriginalDraw = originalDraw;
     DevPanel.draw = function (ctx) {
       if (!GAME_CONFIG.adminTuningEnabled) return;
       if (!this.open || this.tab !== 'HITBOXES') { originalDraw.call(this, ctx); return; }
@@ -325,7 +392,7 @@
       ctx.fillText('DEVELOPER PANEL', panel.x + 22, panel.y + 38);
       ctx.font = '13px Arial';
       ctx.fillStyle = '#aaa';
-      ctx.fillText('Hitbox editor: choose entity, choose rectangle, tune x/y/w/h, SAVE, then EXPORT.', panel.x + 22, panel.y + 58);
+      ctx.fillText('Hitbox editor: BODY receives damage, ATTACK deals damage, PUSHBOX is only movement collision.', panel.x + 22, panel.y + 58);
       this.drawButton(ctx, panel.x + panel.w - 78, panel.y + 14, 56, 32, 'X');
       this.drawTabs(ctx);
       this.drawHitboxEditor(ctx);
@@ -369,13 +436,13 @@
 
       ctx.fillStyle = '#aaa';
       ctx.font = '12px Arial';
-      ctx.fillText('Tip: x/y are offsets from feet anchor. w/h are rectangle size. Positive x goes forward when facing right.', r.box.x + 24, r.box.y + 370);
-      ctx.fillText('After tuning: SAVE stores locally. EXPORT copies JSON to console/clipboard for permanent config update.', r.box.x + 24, r.box.y + 392);
+      ctx.fillText('BODY + ATTACK are combat. PUSHBOX is only for spacing/separation.', r.box.x + 24, r.box.y + 370);
+      ctx.fillText('SAVE stores locally. EXPORT prints hitboxes so they can be copied into permanent config.', r.box.x + 24, r.box.y + 392);
     };
 
     DevPanel.drawHitboxSlider = function (ctx, prop, value, x, y) {
-      const min = prop === 'w' || prop === 'h' ? 4 : -220;
-      const max = prop === 'w' || prop === 'h' ? 260 : 220;
+      const min = prop === 'w' || prop === 'h' ? 4 : -240;
+      const max = prop === 'w' || prop === 'h' ? 280 : 240;
       const ratio = (value - min) / (max - min);
       ctx.fillStyle = '#fff';
       ctx.font = 'bold 14px Arial';
@@ -426,13 +493,14 @@
       return images.enemies && images.enemies[entity.key] && images.enemies[entity.key].idle;
     };
 
-    const originalExportConfig = DevPanel.exportConfig;
+    const originalExportConfig = DevPanel.hitboxOriginalExportConfig || DevPanel.exportConfig;
+    DevPanel.hitboxOriginalExportConfig = originalExportConfig;
     DevPanel.exportConfig = function () {
       originalExportConfig.call(this);
       console.log('STREETS_OF_RUSSIA_HITBOXES_ONLY');
       console.log(JSON.stringify(GAME_CONFIG.hitboxes, null, 2));
     };
 
-    DevPanel.hitboxEditorPatchApplied = true;
+    DevPanel.hitboxEditorSimplePatchApplied = true;
   }
 })();
