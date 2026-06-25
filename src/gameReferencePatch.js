8467a1054bc95d3d97bc5dc45937355a34df6c06
miyadind <miyadind@gmail.com>
2026-06-25 12:07:03 -0700

    Load analytics patch dynamically

--- src/gameReferencePatch.js
+++ src/gameReferencePatch.js
@@ -1,6 +1,17 @@
 (function () {
   if (typeof GameApp === 'undefined') return;
 
+  function loadAnalyticsPatch() {
+    if (document.querySelector('script[data-local-analytics="true"]')) return;
+    const script = document.createElement('script');
+    script.src = 'src/analyticsPatch.js?v=45d601f';
+    script.async = false;
+    script.dataset.localAnalytics = 'true';
+    document.body.appendChild(script);
+  }
+
+  loadAnalyticsPatch();
+
   const previousInit = GameApp.prototype.init;
   GameApp.prototype.init = async function () {
     window.game = this;
