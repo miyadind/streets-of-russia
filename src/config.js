0f2b65630b2d4506024e5c8de5fdd21b793a3362
Den Mii <denisflexam@gmail.com>
2026-08-03 20:58:38 -0700

    Scale down NEgay final whip frame

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const GAME_CONFIG = {
-  "buildVersion": "0.4.163",
+  "buildVersion": "0.4.164",
   "width": 1280,
   "height": 720,
   "targetFPS": 60,
@@ -367,7 +367,7 @@ const GAME_CONFIG = {
       "damage": 16,
       "appearSoundPath": "assets/enemies/NEgay/appear.mp3?v=negay-appear-2",
       "scale": 0.12,
-      "finalAttackScale": 1.5,
+      "finalAttackScale": 1.275,
       "attackDamageSource": "ranged",
       "minDistanceX": 105,
       "preferredDistanceX": 175,
--- src/game.bundle.js
+++ src/game.bundle.js
@@ -6,7 +6,7 @@
 
 /* ===== src/config.js ===== */
 const GAME_CONFIG = {
-  "buildVersion": "0.4.163",
+  "buildVersion": "0.4.164",
   "width": 1280,
   "height": 720,
   "targetFPS": 60,
@@ -374,7 +374,7 @@ const GAME_CONFIG = {
       "damage": 16,
       "appearSoundPath": "assets/enemies/NEgay/appear.mp3?v=negay-appear-2",
       "scale": 0.12,
-      "finalAttackScale": 1.5,
+      "finalAttackScale": 1.275,
       "attackDamageSource": "ranged",
       "minDistanceX": 105,
       "preferredDistanceX": 175,
