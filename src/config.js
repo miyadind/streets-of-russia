7d76b0500f7fc4599a9096847d85b8a3b16615dc
Den Mii <denisflexam@gmail.com>
2026-09-08 23:41:46 -0700

    Remove white fringe from NEgay sprites

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const GAME_CONFIG = {
-  "buildVersion": "0.4.323",
+  "buildVersion": "0.4.324",
   "width": 1280,
   "height": 720,
   "targetFPS": 60,
--- src/game.bundle.js
+++ src/game.bundle.js
@@ -6,7 +6,7 @@
 
 /* ===== src/config.js ===== */
 const GAME_CONFIG = {
-  "buildVersion": "0.4.323",
+  "buildVersion": "0.4.324",
   "width": 1280,
   "height": 720,
   "targetFPS": 60,
